822f5b204fe2113182a289e338af1b3b4b1622aa
John-zu <angel_neet@outlook.com>
2024-04-27 20:02:19 -0600

    Clase de leyout completa

---
+++ Bases/Interfaz-Basica/src/main/java/com/ib/interfazbasica/LayoutBorderPaneEjemplo.java
@@ -0,0 +1,52 @@
+package com.ib.interfazbasica;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+public class LayoutBorderPaneEjemplo extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn1 = new Button("Button1");
+        Button btn2 = new Button("Button2");
+        Button btn3 = new Button("Button3");
+        Button btn4 = new Button("Button4");
+        Button btn5 = new Button("Button5");
+
+        BorderPane root = new BorderPane();
+
+        root.setCenter(btn1);
+        btn1.setMaxWidth(Double.MAX_VALUE);
+        btn1.setMaxHeight(Double.MAX_VALUE);
+        root.setMargin(btn1, new Insets(5, 5, 5, 5));
+
+        root.setTop(btn2);
+        btn2.setMaxWidth(Double.MAX_VALUE);
+        btn2.setMaxHeight(Double.MAX_VALUE);
+
+        root.setBottom(btn3);
+        btn3.setMaxWidth(Double.MAX_VALUE);
+        btn3.setMaxHeight(Double.MAX_VALUE);
+
+        root.setLeft(btn4);
+        btn4.setMaxWidth(Double.MAX_VALUE);
+        btn4.setMaxHeight(Double.MAX_VALUE);
+        root.setMargin(btn4, new Insets(5, 0, 5, 0));
+
+        root.setRight(btn5);
+        btn5.setMaxWidth(Double.MAX_VALUE);
+        btn5.setMaxHeight(Double.MAX_VALUE);
+        root.setMargin(btn5, new Insets(5, 0, 5, 0));
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("LayoutBorderPaneEjemplo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Bases/Interfaz-Basica/src/main/java/com/ib/interfazbasica/README.md
@@ -0,0 +1,17 @@
+# Información del código.
+En esta area hablaremos sobre cosas que estamos usando en el codigo y cada cosa que afectua en este. Hablaremos principalmente de conceptos que no sean cosas de informacion basicas del lenguaje, si no del funcionamiento de JavaFX.
+
+## Propiedades que usaremos.
+#### 1.- Border pane.
+Es en si una clase para layout. este dara acmodos en la ventana que se mostrara.
+
+Se puede hacer ver en los ejemplos de botones.
+ 
+#### 2.- Button.
+Lo que hicimos en esta practica con los botones fue lo siguiente: 
+##### -Direccionamiento.
+Usando las propiedades como top, bottom, left y rigth le dimos una direccion donde estos podrian posisionarse.
+##### -Exponcion.
+Con una propidad que vemos de max_value podemos hacer que este siempre tome el mayor valor posible dentor de su hare que se halle, esto hara que se pueda autoexpandir dependiendo del tamaño de la ventana.
+##### -Margenes.
+Esta propiedad sirve para que los botones no esten tan pegados o al menos no se vean que estan casi tocandose entre ellos y apenas hay espacio entre ellos.
