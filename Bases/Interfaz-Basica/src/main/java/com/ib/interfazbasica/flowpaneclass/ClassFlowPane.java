d19b2b38647b797f5e826edb716e230883ed27cd
John-zu <angel_neet@outlook.com>
2024-04-28 02:08:00 -0600

    Ingreso del fin de clase de flowpane

--- Bases/Interfaz-Basica/src/main/java/com/ib/interfazbasica/flowpaneclass/ClassFlowPane.java
+++ Bases/Interfaz-Basica/src/main/java/com/ib/interfazbasica/flowpaneclass/ClassFlowPane.java
@@ -1,13 +1,25 @@
 package com.ib.interfazbasica.flowpaneclass;
 
 import javafx.application.Application;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class ClassFlowPane extends Application {
     public void start(Stage primaryStage) {
+
+        Button btn[] = new Button[15];
         FlowPane root = new FlowPane();
+        root.setOrientation(Orientation.VERTICAL);
+        root.setHgap(10);
+        root.setVgap(10);
+
+        for(int i = 0; i < btn.length; i++){
+            btn[i] = new Button("Boton " + (i+1));
+            root.getChildren().add(btn[i]); 
+        }
 
         Scene scene = new Scene(root, 300, 250);
         primaryStage.setTitle("Clase FlowPane");
--- Bases/Interfaz-Basica/src/main/java/com/ib/interfazbasica/flowpaneclass/README.md
+++ Bases/Interfaz-Basica/src/main/java/com/ib/interfazbasica/flowpaneclass/README.md
@@ -1,3 +1,14 @@
 # Infromación de la practica 
 En esta ocacion hablaremos sobre flowpane que servira como una combinacion entre vertical y horizontal pane, servira para guardar de manera mas flexible el nodos que tengamos(objetos como botones, textfields, propiedades)
 
+## Mas sobre Flowpane
+Este metodo hace que las cosas se acomoden en horizontal de manera automatica, entonces depende del tamaño del largo de la ventana.
+
+Las dimenciones dependen un demaciado de la dimensión.
+
+Otra cosa a remarcar muy importante es que flowpane tiene 6 propiedades con las que puede trabajar, de momento veremos las siguientes tres propiedades:
+- setOrientation
+sirve para cambiar la horientacion.
+- setHgap: Dar espacio horizontal. 
+- setVgap: Dar espacio verticualmente.
+
