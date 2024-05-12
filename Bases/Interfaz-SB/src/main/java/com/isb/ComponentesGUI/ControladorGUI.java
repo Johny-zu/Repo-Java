13bb4df71a53ee106c27b05bd031a04840e0be5b
John-zu <angel_neet@outlook.com>
2024-05-11 23:40:50 -0600

    TExtField prueba GUI

--- Bases/Interfaz-SB/src/main/java/com/isb/ComponentesGUI/ControladorGUI.java
+++ Bases/Interfaz-SB/src/main/java/com/isb/ComponentesGUI/ControladorGUI.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import javax.swing.*;
 import java.net.URL;
@@ -16,6 +18,28 @@ public class ControladorGUI implements Initializable {
 
     @FXML
     private Label menBTN;
+
+    @FXML
+    private TextField txt1, txt2, txt3, txt4;
+
+    @FXML
+    private void eventKey(KeyEvent event){
+        Object evt = event.getSource();
+        if (evt.equals(txt1)){
+            if(! Character.isLetter(event.getCharacter().charAt(0)))
+                event.consume();
+        } else if(evt.equals(txt2)) {
+            if(! Character.isDigit(event.getCharacter().charAt(0)))
+                event.consume();
+        }else if(evt.equals(txt3)) {
+            if(! Character.isDigit(event.getCharacter().charAt(0)) && ! Character.isLetter(event.getCharacter().charAt(0)))
+                event.consume();
+        }else if(evt.equals(txt4)) {
+            if (txt4.getText().length() < 9)
+                event.consume();
+        }else
+            System.out.print("Algo anda fallando");
+    }
 
     @FXML
     private void eventButton(ActionEvent event){
